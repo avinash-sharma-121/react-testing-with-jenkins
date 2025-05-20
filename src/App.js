1167ff8e650fd6e82a5147fac6e9d686b6ba7b41
Avinash Kumar Sharma <134773362+avinash-sharma-121@users.noreply.github.com>
2025-05-20 22:56:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Avinash kumar Sharma
+          Avinash kumar
         </p>
         <a
           className="App-link"
